ed53d407e82c9c4a28e638df08dd6f4059b8ad38
Amir Khan <khansourav119@gmail.com>
2025-12-05 22:32:39 +0600

    project setup

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="bg-blue-600 text-white p-4">
+      <h2 className="text-lg font-bold">Public Issue System</h2>
+    </div>
+  );
+};
+
+export default Navbar;
